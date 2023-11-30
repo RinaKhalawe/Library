ae0c0ee8db0784caf6c38ec9a863961aab10b846
Rina Khalawe <211102@ppu.edu.ps>
2023-11-30 22:52:47 +0300

    first second

--- LibraryProject/src/main/java/com/mycompany/libraryproject/LibraryProject.java
+++ LibraryProject/src/main/java/com/mycompany/libraryproject/LibraryProject.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.libraryproject;
 
+//import java.time.LocalDate;
+  import java.util.Scanner;
+
 /**
  *
  * @author TTC
@@ -11,6 +14,61 @@ package com.mycompany.libraryproject;
 public class LibraryProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to my library ");
+        Book book = new Book();
+        Library l1 = new Library();
+       // String result;
+        
+        l1.addBook(book);
+        
+        Library library = new Library();
+        Scanner input = new Scanner(System.in);
+        int choice ;
+        System.out.println("The Library Menu :");
+        System.out.println("1- Add Books");
+        System.out.println("2- Add Students ");
+        
+        do {
+        
+        System.out.print("Enter your choice: ");
+        choice = input.nextInt();
+        
+        switch(choice) {
+        case 1 -> {
+        System.out.print("Enter book title: ");
+        String title = input.next();
+        System.out.print("Enter book number: ");
+        String author = input.next();
+        System.out.print("Enter book genre: ");
+        int number = input.nextInt();
+        System.out.print("Enter book version: ");
+        String genre = input.next();
+        System.out.print("Enter book author: ");
+        int version = input.nextInt();
+        System.out.print("Enter book date (dd-MM-yyyy): ");
+        String date = input.next();
+        
+        
+        library.addBook(new Book(title,number,genre,version,author, date));
+        System.out.println("Book added successfully!");
+        }
+        case 2 -> {
+        System.out.print("Enter student id: ");
+        int studentId = input.nextInt();
+        System.out.print("Enter student name: ");
+        String studentName = input.next();
+        System.out.print("Enter student address: ");
+        String studentAddr = input.next();
+        System.out.print("Enter student birth date (yyyy-MM-dd): ");
+        String studentBirthDate = input.next();
+        
+        System.out.print("Enter student major: ");
+        String studentMajor = input.next();
+        
+        library.addStudent(new Student(studentId, studentName, studentAddr, studentMajor, studentBirthDate));
+        System.out.println("Student added successfully!");
+        }
     }
 }
+
+ 
